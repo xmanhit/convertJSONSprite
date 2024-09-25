a13644fee2722f4d5c4dd940c4822ea4eb7122f0
Nguyễn Xuân Mạnh <57929106+xmanhit@users.noreply.github.com>
2024-09-25 14:20:12 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,87 +4,196 @@ import ReactJson from 'react-json-view'
 import './App.css'
 
 function App() {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [file, setFile] = useState<string>("")
-  const [fileProcessed, setFileProcessed] = useState<string>("")
-  const [fileName, setFileName] = useState("")
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [file, setFile] = useState<string>("")
+    const [fileProcessed, setFileProcessed] = useState<string>("")
+    const [fileName, setFileName] = useState("")
 
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const fileReader = new FileReader();
-    if (e.target.files) {
-      fileReader.readAsText(e.target.files[0], "UTF-8");
-      fileReader.onload = e => {
-        if (e.target?.result) {
-          const result = e.target.result.toString()
-          const json = JSON.parse(result)
-          const filename = Object.keys(json.mc)[0];          
-          const subTextures = Object.entries(json.res).map(([name, {x, y, w, h}]: any) => ({
-            name,
-            width: w,
-            height: h,
-            frameX: json.mc[filename].frames.find(({res}: any) => res == name).x,
-            frameY: json.mc[filename].frames.find(({res}: any) => res == name).y,
-            x,
-            y
-          }));
-          
-          
-          const resultJSON = {
-            SubTexture: subTextures,
-            imagePath: filename + ".png",
-            name: filename
-          };
-          setFile(result);
-          setFileProcessed(JSON.stringify(resultJSON, null, 2))
-          setFileName(filename)
-        }
-      };
-    };
-  }
+    const inputRef2 = useRef<HTMLInputElement>(null);
+    const [file2, setFile2] = useState<string>("")
+    const [fileProcessed2, setFileProcessed2] = useState<string>("")
+    const [fileName2, setFileName2] = useState("")
 
-  const resetFileInput = () => {
-    if (inputRef.current) {
-      inputRef.current.value = "";
-      setFile("");
-      setFileProcessed("");
+
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const fileReader = new FileReader();
+        if (e.target.files) {
+            fileReader.readAsText(e.target.files[0], "UTF-8");
+            fileReader.onload = e => {
+                if (e.target?.result) {
+                    const result = e.target.result.toString()
+                    const json = JSON.parse(result)
+                    const filename = Object.keys(json.mc)[0];
+                    const subTextures = Object.entries(json.res).map(([name, { x, y, w, h }]: any) => ({
+                        name,
+                        width: w,
+                        height: h,
+                        frameX: json.mc[filename].frames.find(({ res }: any) => res == name).x,
+                        frameY: json.mc[filename].frames.find(({ res }: any) => res == name).y,
+                        x,
+                        y
+                    }));
+
+
+                    const resultJSON = {
+                        SubTexture: subTextures,
+                        imagePath: filename + ".png",
+                        name: filename
+                    };
+                    setFile(result);
+                    setFileProcessed(JSON.stringify(resultJSON, null, 2))
+                    setFileName(filename)
+                }
+            };
+        };
     }
-  }
 
-  const downloadFile = ({ data, fileName, fileType }: any) => {
-    // Create a blob with the data we want to download as a file
-    const blob = new Blob([data], { type: fileType })
-    // Create an anchor element and dispatch a click event on it
-    // to trigger a download
-    const a = document.createElement('a')
-    a.download = fileName + ".json"
-    a.href = window.URL.createObjectURL(blob)
-    const clickEvt = new MouseEvent('click', {
-      view: window,
-      bubbles: true,
-      cancelable: true,
-    })
-    a.dispatchEvent(clickEvt)
-    a.remove()
-  }
+    const resetFileInput = () => {
+        if (inputRef.current) {
+            inputRef.current.value = "";
+            setFile("");
+            setFileProcessed("");
+        }
+    }
 
-  return (
-    <div>
-      <div>
-        <input ref={inputRef} type="file" id="newFile" onChange={handleFileChange} />
-        <button type="button" onClick={resetFileInput}>Clear</button>
-        <button disabled={!fileProcessed} type="button" onClick={() => downloadFile({data: fileProcessed, fileName, fileType:"text/json"})}>Download</button>
-      </div>
+    const downloadFile = ({ data, fileName, fileType }: any) => {
+        // Create a blob with the data we want to download as a file
+        const blob = new Blob([data], { type: fileType })
+        // Create an anchor element and dispatch a click event on it
+        // to trigger a download
+        const a = document.createElement('a')
+        a.download = fileName + ".json"
+        a.href = window.URL.createObjectURL(blob)
+        const clickEvt = new MouseEvent('click', {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+        })
+        a.dispatchEvent(clickEvt)
+        a.remove()
+    }
 
-      <div style={{display: "flex"}}>
-        <div>
-          {file && <ReactJson src={JSON.parse(file)} theme="monokai" />}
-        </div>
-        <div>
-          {fileProcessed && <ReactJson src={JSON.parse(fileProcessed)} theme="monokai" />}
-        </div>
-      </div>
-    </div>
-  )
+
+    const handleFileChange2 = (e: ChangeEvent<HTMLInputElement>) => {
+        const fileReader = new FileReader();
+        if (e.target.files) {
+            fileReader.readAsText(e.target.files[0], "UTF-8");
+            fileReader.onload = e => {
+                if (e.target?.result) {
+                    const result = e.target.result.toString()
+                    const json = JSON.parse(result)
+                    const filename = Object.keys(json.mc)[0];
+
+                    const resultJSON = {
+                        mc: {
+                            [fileName]: {
+                                frames: [],
+                                frameRate: 3 // Assuming frameRate is 3 based on "frame": 3 in bw2.json
+                            }
+                        },
+                        res: {}
+                    };
+                    
+                    json.frames.forEach((frame, index) => {
+                        // Add frame data to mc.uititle00053_000.frames
+                        result.mc.[fileName].frames.push({
+                          res: index.toString().padStart(2, '0'),
+                          y: -frame.oy,
+                          x: -frame.ox
+                        });
+                    
+                        // Add resource data to res
+                        result.res[index.toString().padStart(2, '0')] = {
+                          h: frame.h,
+                          y: frame.y,
+                          w: frame.w,
+                          x: frame.x
+                        };
+                      });
+
+                    setFile2(result);
+                    setFileProcessed2(JSON.stringify(resultJSON, null, 2))
+                    setFileName2(filename)
+                }
+            };
+        };
+    }
+
+    const resetFileInput2 = () => {
+        if (inputRef.current) {
+            inputRef.current.value = "";
+            setFile2("");
+            setFileProcessed2("");
+        }
+    }
+
+    const downloadFile2 = ({ data, fileName, fileType }: any) => {
+        // Create a blob with the data we want to download as a file
+        const blob = new Blob([data], { type: fileType })
+        // Create an anchor element and dispatch a click event on it
+        // to trigger a download
+        const a = document.createElement('a')
+        a.download = fileName + ".json"
+        a.href = window.URL.createObjectURL(blob)
+        const clickEvt = new MouseEvent('click', {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+        })
+        a.dispatchEvent(clickEvt)
+        a.remove()
+    }
+
+    return (
+        <main>
+            <div>
+                <h1>JSON to TexturePacker Converter</h1>
+                <div>
+                    <label htmlFor="newFile">Upload JSON file:</label>
+                    <input ref={inputRef} type="file" id="newFile" onChange={handleFileChange} />
+                    <button type="button" onClick={resetFileInput}>Clear</button>
+                    <button disabled={!fileProcessed} type="button"
+                        onClick={
+                            () => downloadFile({
+                                data: fileProcessed, fileName, fileType: "text/json"
+                            })}
+                    >Download</button>
+                </div>
+
+                <div style={{ display: "flex" }}>
+                    <div>
+                        {file && <ReactJson src={JSON.parse(file)} theme="monokai" />}
+                    </div>
+                    <div>
+                        {fileProcessed && <ReactJson src={JSON.parse(fileProcessed)} theme="monokai" />}
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h1>JSON to TexturePacker Converter 2</h1>
+                <div>
+                    <label htmlFor="newFile2">Upload JSON file:</label>
+                    <input ref={inputRef2} type="file" id="newFile2" onChange={handleFileChange2} />
+                    <button type="button" onClick={resetFileInput2}>Clear</button>
+                    <button disabled={!fileProcessed2} type="button"
+                        onClick={
+                            () => downloadFile2({
+                                data: fileProcessed2, fileName2, fileType: "text/json"
+                            })}
+                    >Download</button>
+                </div>
+
+                <div style={{ display: "flex" }}>
+                    <div>
+                        {file2 && <ReactJson src={JSON.parse(file2)} theme="monokai" />}
+                    </div>
+                    <div>
+                        {fileProcessed2 && <ReactJson src={JSON.parse(fileProcessed2)} theme="monokai" />}
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
 }
 
 export default App
